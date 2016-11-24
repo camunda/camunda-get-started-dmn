156d00d132363eba056ec08af6ac2c86045d7119
Philipp Ossler <philipp.ossler@camunda.com>
2016-11-24 08:55:53 +0100

    Step 4: Add PostDeploy method

--- src/main/java/org/camunda/bpm/getstarted/dmn/DinnerApplication.java
+++ src/main/java/org/camunda/bpm/getstarted/dmn/DinnerApplication.java
@@ -16,11 +16,36 @@
  */
 package org.camunda.bpm.getstarted.dmn;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.camunda.bpm.application.PostDeploy;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.JakartaServletProcessApplication;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.engine.DecisionService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
 
 @ProcessApplication("Dinner App DMN")
 public class DinnerApplication extends JakartaServletProcessApplication
 {
-  //empty implementation
+    protected final static Logger LOGGER = Logger.getLogger(DinnerApplication.class.getName());
+
+    @PostDeploy
+    public void evaluateDecisionTable(ProcessEngine processEngine) {
+
+      DecisionService decisionService = processEngine.getDecisionService();
+
+      VariableMap variables = Variables.createVariables()
+        .putValue("season", "Spring")
+        .putValue("guestCount", 10);
+
+      DmnDecisionTableResult dishDecisionResult = decisionService.evaluateDecisionTableByKey("dish", variables);
+      String desiredDish = dishDecisionResult.getSingleEntry();
+
+      LOGGER.log(Level.INFO, "\n\nDesired dish: {0}\n\n", desiredDish);
+    }
+
 }
